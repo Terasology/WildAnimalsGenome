acfc45b9b1f1de3c13b3b1a75a41ddbc0006a831
darkweird <foxflameless@gmail.com>
2021-09-07 17:55:25 +0300

    feature(gestaltv7-events): replace TS's Event to gestaltv7's Event

--- src/main/java/org/terasology/wildAnimalsGenome/event/ActivateMatingScreenEvent.java
+++ src/main/java/org/terasology/wildAnimalsGenome/event/ActivateMatingScreenEvent.java
@@ -3,8 +3,8 @@
 package org.terasology.wildAnimalsGenome.event;
 
 import org.terasology.engine.entitySystem.entity.EntityRef;
-import org.terasology.engine.entitySystem.event.Event;
 import org.terasology.engine.network.OwnerEvent;
+import org.terasology.gestalt.entitysystem.event.Event;
 
 @OwnerEvent
 public class ActivateMatingScreenEvent implements Event {
--- src/main/java/org/terasology/wildAnimalsGenome/event/MatingActivatedEvent.java
+++ src/main/java/org/terasology/wildAnimalsGenome/event/MatingActivatedEvent.java
@@ -3,9 +3,9 @@
 package org.terasology.wildAnimalsGenome.event;
 
 import org.terasology.engine.entitySystem.entity.EntityRef;
-import org.terasology.engine.entitySystem.event.Event;
 import org.terasology.engine.network.Replicate;
 import org.terasology.engine.network.ServerEvent;
+import org.terasology.gestalt.entitysystem.event.Event;
 
 /**
  * This event is sent when mating is activated.
