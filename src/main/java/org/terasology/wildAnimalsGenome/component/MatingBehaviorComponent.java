2796e0ca72ec36ecfa16d58b5c19c8a1af908df1
Zangus <wong.thi@gmail.com>
2024-01-03 19:27:24 +0100

    refactor: Components without attributes to EmptyComponent (#19)
    Co-authored-by: thierrywong <wong.t@sfeir.com>

--- src/main/java/org/terasology/wildAnimalsGenome/component/MatingBehaviorComponent.java
+++ src/main/java/org/terasology/wildAnimalsGenome/component/MatingBehaviorComponent.java
@@ -2,14 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.wildAnimalsGenome.component;
 
-import org.terasology.gestalt.entitysystem.component.Component;
+import org.terasology.gestalt.entitysystem.component.EmptyComponent;
 
 /**
  * This component is added when an animal's mating behavior is changed to mate.
  */
-public class MatingBehaviorComponent implements Component<MatingBehaviorComponent> {
-    @Override
-    public void copyFrom(MatingBehaviorComponent other) {
-
-    }
+public class MatingBehaviorComponent extends EmptyComponent<MatingBehaviorComponent> {
 }
